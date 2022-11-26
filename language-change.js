999152f6a86f5133d7b6bf5b7e0d80c3636b7ee9
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-11-26 22:04:31 +0100

    fixed language switch

--- language-change.js
+++ language-change.js
@@ -1,4 +1,12 @@
+const hiddenLanguage = document.querySelector(".hidden-text");
+const english = document.querySelectorAll(".eng");
+const swedish = document.querySelectorAll(".sve");
+
 let eng = true;
+
+swedish.forEach((element) => {
+  element.classList.add("hidden-text");
+});
 
 language.forEach((button) => {
   const newbutton = button;
@@ -8,10 +16,22 @@ language.forEach((button) => {
       language.forEach((button) => {
         button.innerHTML = "<a>SV<strong>/EN</strong></a>";
       });
+      swedish.forEach((element) => {
+        element.classList.add("hidden-text");
+      });
+      english.forEach((element) => {
+        element.classList.remove("hidden-text");
+      });
       eng = true;
     } else {
       language.forEach((button) => {
         button.innerHTML = "<a><strong>SV/</strong>EN</a>";
+      });
+      swedish.forEach((element) => {
+        element.classList.remove("hidden-text");
+      });
+      english.forEach((element) => {
+        element.classList.add("hidden-text");
       });
       eng = false;
     }
